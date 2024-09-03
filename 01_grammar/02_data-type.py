fcc28d5df78c93b4c5c87949f04f66e786cfb0e9
YoonJae <realyoon77@gmail.com>
2024-09-03 22:43:10 +0900

    [2024-09-03] 공부

--- 01_grammar/02_data-type.py
+++ 01_grammar/02_data-type.py
@@ -1,1 +1,27 @@
 print('hello \a world')
+
+# 문자열
+
+# 1. 큰따옴표로 양쪽 둘러싸기
+"Hello World"
+"he's name is "
+# 2. 작은따옴표로 양쪽 둘러싸기
+'"Python is fun"'
+# 3. 큰따옴표 3개를 연속으로 써서 양쪽 둘러싸기
+"""Life is too short, You need python"""
+# 4. 작은따옴표 3개를 연속으로 써서 양쪽 둘러싸기
+'''Life is too short, You need python'''
+
+food = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\" he says."
+
+
+
+# List
+
+odd = [1, 3, 5, 7, 9]
+
+# 삼중 리스트
+threeA = [1, 2, ['a', 'b', ['Life', 'is']]]
+
+print(threeA[2][2][0])
