4fc8758d39e7f9101806dd93c6831b0aa67c4c8e
YoonJae <realyoon77@gmail.com>
2024-09-06 23:02:16 +0900

    [2024-09-06] 공부

--- 01_grammar/02_data-type.py
+++ 01_grammar/02_data-type.py
@@ -25,3 +25,14 @@ odd = [1, 3, 5, 7, 9]
 threeA = [1, 2, ['a', 'b', ['Life', 'is']]]
 
 print(threeA[2][2][0])
+
+
+## 함수
+
+a_list = [3, 4, 62, 27, 83, 956, 26, 58, 3, 78, 168, 64, 78]
+
+def print_list(a):  # 지금부터 print_list 함수를 만들겠다는 뜻
+    for i in a:
+        print(i)
+
+print_list(a_list)
